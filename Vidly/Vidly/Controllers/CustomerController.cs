23924f13cd17abb996eab9ec52253ade2367ff37
Vu Viet Ha <havv56@gmail.com>
2018-08-01 18:40:30 +0700

    Adding and Styling Validation _44

--- Vidly/Vidly/Controllers/CustomerController.cs
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -53,6 +53,18 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save( CustomerModel customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+
+                };
+                return View("CustomerForm", viewModel);
+
+            }
+                
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);

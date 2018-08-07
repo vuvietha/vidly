19ef458545c6a41b9f9b8c26cdf9744d2ed01421
Vu Viet Ha <havv56@gmail.com>
2018-08-07 11:22:21 +0700

    Create role using add code in register method in account controller and then register in view. The user will have role created in code in account controller

--- Vidly/Vidly/Controllers/AccountController.cs
+++ Vidly/Vidly/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Vidly.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Vidly.Controllers
 {
@@ -155,6 +156,12 @@ namespace Vidly.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //Temp code
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    await roleManager.CreateAsync(new IdentityRole("CanManageMovie"));
+                    await UserManager.AddToRoleAsync(user.Id, "CanManageMovie");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
